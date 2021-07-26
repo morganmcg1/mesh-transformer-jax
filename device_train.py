3759018fc0eba57cee9a44ab1dd69106eb8bd0a9
Morgan McGuire <morganmcg1@users.noreply.github.co>
2021-07-26 13:11:19 +0100

    add_file now uses artifact

--- device_train.py
+++ device_train.py
@@ -70,7 +70,7 @@ def save(network, step, bucket, path, mp, aux=None, keep_n=3, delete_old=True, w
         write_ckpt(network.state, f"gs://{bucket}/{path}/step_{step}/", shard_id)
         # Write artifacts file
         if wandb_artifacts is not None:
-            wandb.add_file(f"gs://{bucket}/{path}/step_{step}/{shard_id}")
+            wandb_artifacts[shard_id].add_file(f"gs://{bucket}/{path}/step_{step}/{shard_id}")
             wandb.log_artifact(wandb_artifacts[shard_id], aliases=[f'step={step}',f'shard={shard_id}'])
 
     print(f"Wrote checkpoint in {time.time() - start:.06}s")
