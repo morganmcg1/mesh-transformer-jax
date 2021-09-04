2a42052dbab0d89b1e24a4f9768b020af1f9891d
Morgan McGuire <morganmcg1@users.noreply.github.co>
2021-09-04 12:55:52 +0100

    fixes prompt path

--- device_train.py
+++ device_train.py
@@ -364,8 +364,8 @@ if __name__ == "__main__":
 
             # read prompts and create path to save generations to
             from pathlib import Path
-            prompts_file = params['prompts_filename']
-            prompts_df = pd.read_csv(f'{model_dir}/{prompts_file}')
+            prompts_path = params['prompts_path']
+            prompts_df = pd.read_csv(f'"gs://{bucket}/{prompts_path}') 
             generations_path = Path(f'{model_dir}/generated_samples').mkdir(parents=True, exist_ok=True) 
 
             # create W&B Table to log generations to
